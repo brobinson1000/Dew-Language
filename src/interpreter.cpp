31e3fc958205213f641d5794cbe9487853ec823e
brobinson1000 <1802brad@gmail.com>
2026-03-14 09:16:07 -0400

    add heap allocated moves

---
+++ src/interpreter.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <functional>
+#include <variant>
+#include <memory>
+#include <vector>
+#include <algorithm>
+
+
+
+std::unordered_map<std::string, std::variant<int, double, std::string>> variables;
+
+using varType = std::variant<int, double, std::string>;
+std::unordered_map<std::string, varType*> heap;
+
+
+void displayCommand(std::istringstream& iss) {
+    std::string word;
+    iss >> word;
+
+    auto it = heap.find(word);
+    if (it != heap.end()) {
+        auto &var = *(it->second);
+        if (std::holds_alternative<int>(var)) { 
+            std::cout << std::get<int>(var); 
+        } else if(std::holds_alternative<double>(var)) { 
+            std::cout << std::get<double>(var);
+        } else { 
+            std::cout << std::get<std::string>(var);
+
+            std::cout << "\n";
+            return;
+        }
+    }
+
+    auto it_ = variables.find(word);
+    if ( it_ != variables.end()) {
+        auto &var = it_->second;
+        if (std::holds_alternative<int>(var)) { 
+            std::cout << std::get<int>(var); 
+        } else if (std::holds_alternative<double>(var)) {  
+            std::cout << std::get<double>(var); 
+        } else { 
+            std::cout << std::get<std::string>(var);
+        
+            std::cout << "\n";
+            return;   
+        }
+
+        std::string rest;
+        std::getline(iss, rest);
+        std::cout << word << rest << "\n";
+
+
+    }   
+
+}
+
+void moveCommand(std::istringstream& iss) {
+    std::string val, dest;
+    iss >> val >> dest;
+    auto search = variables.find(val);
+    if (search != variables.end()) {
+        variables[dest] = search->second;
+    } else {
+        variables[dest] = val;
+    }   
+}    
+
+void movehCommand(std::istringstream& iss) {
+    std::string val, dest;
+    iss >> val >> dest;
+
+    auto search = heap.find(val);
+    if ( search != heap.end()) {
+        heap[dest] = new varType(*(search->second));
+    } else {
+        heap[dest] = new varType(val);
+    }
+
+
+
+
+}
+
+
+void getCommand(std::istringstream& iss) {
+    std::string varName;
+    iss >> varName;
+
+    if (varName.empty()) {
+        std::cerr << "ERROR: No variable specified\n";
+        return;
+    }
+
+    std::string inputValue;
+
+    std::getline(std::cin, inputValue);
+
+    // Detect numeric type
+    try {
+        variables[varName] = std::stoi(inputValue);
+        return;
+    } catch(...) {}
+
+    try {
+        variables[varName] = std::stod(inputValue);
+        return;
+    } catch(...) {}
+
+    variables[varName] = inputValue; // fallback to string
+}
+
+
+
+int main() {
+    std::unordered_map<std::string, std::function<void(std::istringstream&)>> commands;
+
+
+    commands["DISPLAY"] = displayCommand;
+    commands["GET"] = getCommand;
+    commands["MOVE"] = moveCommand;
+    commands["MOVEH"] = movehCommand;
+    
+
+    std::string line;
+    while(std::getline(std::cin, line)) {
+        std::istringstream iss(line);
+        std::string cmd;
+        iss >> cmd;
+
+        auto it = commands.find(cmd);
+        if ( it != commands.end()) {
+            it->second(iss);
+        } else {
+            std::cerr << "UNKNOW COMMAND" << cmd << "\n";
+        }
+    }
+}
