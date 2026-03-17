bb9276b643e54e71335e8033f05f6a8b658884ce
brobinson1000 <1802brad@gmail.com>
2026-03-17 17:41:03 -0400

    add display float

--- include/print.h
+++ include/print.h
@@ -6,12 +6,14 @@
 #include <string>
 
 
-using varType = std::variant<double, int, std::string>;
+using varType = std::variant<float ,double, int, std::string>;
 
 extern std::unordered_map<std::string, varType> variables;
 extern std::unordered_map<std::string, varType*> heap;
 
 void displayCommand(std::istringstream& iss);
+
+void display_floatCommand(std::istringstream& iss);
 
 void display_newlineCommand(std::istringstream& iss);
 
