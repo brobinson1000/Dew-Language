90bbcead570b591a227a8f621165594ffd43fcd4
brobinson1000 <1802brad@gmail.com>
2026-03-23 15:22:40 -0400

    add math lib

---
+++ include/math_utils.h
@@ -0,0 +1,28 @@
+#ifndef MATH_UTILS_H
+#define MATH_UTILS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+double math_pi(void);
+
+int math_floor(const char* cmd);
+
+int math_ceil(const char* cmd);
+
+int math_abs(const char* cmd);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+
+
+
