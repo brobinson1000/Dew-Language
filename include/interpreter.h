93899a534afff51327820d887b07386715aa36b4
brobinson1000 <1802brad@gmail.com>
2026-03-28 16:27:27 -0400

    remove the redeclared header functions

--- include/interpreter.h
+++ include/interpreter.h
@@ -3,13 +3,8 @@
 #include <string>
 #include <variant>
 
-using varType = std::variant<int, float, std::string>;
+using varType = std::variant<float, double, int ,std::string>;
 
-void displayFloor(std::istringstream& iss);
-varType floorFunc(std::stringstream& iss);
-void displayCeil(std::istringstream& iss);
-varType ceilFunc(std::istringstream& iss);
-varType piFunc(std::istringstream& iss);
 void moveCommand(std::istringstream& iss);
 void movehCommand(std::istringstream& iss);
 void freehCommand(std::istringstream& iss);
